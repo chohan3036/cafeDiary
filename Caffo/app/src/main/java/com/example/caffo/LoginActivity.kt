0a11471cd126bb1c22ace2737c0f3ea415c23e33
chohan3036 <chohan3036@gmail.com>
2020-08-10 20:48:45 +0900

    navigation created

--- Caffo/app/src/main/java/com/example/caffo/LoginActivity.kt
+++ Caffo/app/src/main/java/com/example/caffo/LoginActivity.kt
@@ -1,12 +1,135 @@
 package com.example.caffo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.*
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.*
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : AppCompatActivity() {
+    var auth : FirebaseAuth? = null
+    var googleSignInClient : GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+        email_login_button.setOnClickListener {
+            signinAndSignup()
+        }
+        google_signin_button.setOnClickListener {
+            //First step
+            googleLogin()
+        }
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
     }
+
+    fun googleLogin(){
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+
+
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+//        if (requestCode == GOOGLE_LOGIN_CODE) {
+//            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+//            try {
+//                // Google Sign In was successful, authenticate with Firebase
+//                val account = task.getResult(ApiException::class.java)!!
+//                println("firebaseAuthWithGoogle:" + account.id)
+//                firebaseAuthWithGoogle(account)
+//            } catch (e: ApiException) {
+//                // Google Sign In failed, update UI appropriately
+//                println("Google sign in failed" + e.toString())
+//                // [START_EXCLUDE]
+//                // [END_EXCLUDE]
+//            }
+//        }
+//    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GOOGLE_LOGIN_CODE){
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result!!.isSuccess){
+                var account = result.signInAccount
+                firebaseAuthWithGoogle(account)
+            }
+        }
+    }
+
+    fun firebaseAuthWithGoogle(account : GoogleSignInAccount?){
+        println("here")
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener {
+                    task ->
+                if(task.isSuccessful){
+                    //Login
+                    moveMainPage(task.result?.user)
+                }
+                else {
+                    //Show the error message
+                    print(task.exception?.message)
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    fun signinAndSignup(){
+        auth?.createUserWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())
+            ?.addOnCompleteListener {
+                task ->
+                if(task.isSuccessful){
+                    //Creating a user account
+                    moveMainPage(task.result?.user)
+                }
+                else if(!task.exception?.message.isNullOrEmpty()){
+                    //Show the error message
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+                else {
+                    //Login if you have an account
+                    siginInEamil()
+                }
+            }
+    }
+
+    fun siginInEamil(){
+        auth?.signInWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())
+            ?.addOnCompleteListener {
+                    task ->
+                if(task.isSuccessful){
+                    //Login
+                    moveMainPage(task.result?.user)
+                }
+                else {
+                    //Show the error message
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    fun moveMainPage(user:FirebaseUser?){
+        if(user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
 }
